d5384359a14cf9367f3f552140d88ad1aaeae19e
moeez999 <moeezraza69@gmail.com>
2023-09-21 18:18:16 +0500

    class added

--- app/ContactUs/page.js
+++ app/ContactUs/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 
-
 function ContactUs() {
   const htmlContentWithScript = `
     <script type="text/javascript" src="https://form.jotform.com/jsform/223421865801049"></script>
@@ -9,6 +8,7 @@ function ContactUs() {
   return (
     <div>
       <div
+        className="iframe-height"
         dangerouslySetInnerHTML={{ __html: htmlContentWithScript }}
       />
     </div>
